01b61700efb43206e9459e3fa52046f5b393a9b8
ss15859 <sam.stockman@bristol.ac.uk>
2024-06-11 20:27:51 +0100

    fix requirements

--- setup.sh
+++ setup.sh
@@ -3,8 +3,3 @@
 # Create the conda environment
 conda env create -f environment.yml;
 
-# Activate earthquakeNPP
-conda activate earthquakeNPP;
-
-# Use pip to install the remaining packages
-pip install -r requirements.txt;
